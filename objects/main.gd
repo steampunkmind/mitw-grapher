d281505181d5b78a7d1f19c8add99e7f6ec21daf
Steampunk Mind <asteampunkmind@gmail.com>
2026-04-07 12:23:24 -0400

    Moves model variables from main node to MITW.

--- objects/main.gd
+++ objects/main.gd
@@ -1,9 +1,7 @@
 extends Node
 
-var _aim_model = ActionInfluenceModel.new()
 var _aim_model_name # holds name between file dialogs
-var _aim_model_json # holds json between file dialogs
-var _gam_model = GovernorActionModel.new()
+var _aim_model_dict # holds dict between file dialogs
 var governor_graphs: Dictionary[String, GovernorGraph]
 
 @export var governor_graph_template: PackedScene
@@ -31,7 +29,7 @@ func _on_file_menu_index_pressed(index) -> void:
 
 func _on_open_files_menu_pressed() -> void:
 	_aim_model_name = null
-	_aim_model_json = null
+	_aim_model_dict = null
 	$FileDialog.set_title("Open an AIM File")
 	$FileDialog.set_filters(["*.aim"])
 	$FileDialog.popup()
@@ -39,24 +37,21 @@ func _on_open_files_menu_pressed() -> void:
 
 func _on_file_dialog_file_selected(path: String) -> void:
 	var file = FileAccess.open(path, FileAccess.READ)
-	var json = JSON.parse_string(file.get_as_text())
+	var dict = JSON.parse_string(file.get_as_text())
 	file.close()
 	
 	if _aim_model_name == null:
 		_aim_model_name = path.get_basename().get_file().capitalize()
-		_aim_model_json = json # Hold data until gam model is selected below.
+		_aim_model_dict = dict # Hold data until gam model is selected below.
 		await get_tree().create_timer(0.5).timeout # Wait for dialog to go away.
 		$FileDialog.set_title("Open a GAM File")
 		$FileDialog.set_filters(["*.gam"])
 		$FileDialog.popup()
 	else:
 		$FileNames.text = _aim_model_name + " - " + path.get_basename().get_file().capitalize()
-		_aim_model.clear_model()
-		_gam_model.clear_model()
-		_aim_model.set_model(_aim_model_json)
-		_gam_model.set_model(json, _aim_model)
+		MITW.set_models(_aim_model_dict, dict)
 		_aim_model_name = null
-		_aim_model_json = null
+		_aim_model_dict = null
 		add_governor_graphs()
 
 
@@ -65,7 +60,7 @@ func add_governor_graphs() -> void:
 	var row_margin = 2
 	var row_location = header_margin
 	var header_width = 0.0
-	for governor: Governor in _gam_model.get_governors():
+	for governor: Governor in MITW.gam_model().get_governors():
 		var graph = governor_graph_template.instantiate()
 		graph.init(governor, row_location)
 		var min_header_width = graph.get_min_header_width()
